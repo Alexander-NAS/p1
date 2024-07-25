dd656a8ffe2cf90b0df5f224cf40e0f4a2af53dc
Alexander-NAS <nas.5@list.ru>
2024-07-26 00:19:56 +0800

    index and about ready

---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import TemplateView
+
+
+class MainView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Bike Store' 
+        return context
+    
+
+
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'О нас' 
+        return context
